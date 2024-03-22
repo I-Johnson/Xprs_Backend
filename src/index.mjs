badfe7d0cb1bf9d26091705a469194941f9f0dbf
Johnson Subedi <subede.johnson@gmail.com>
2024-03-22 16:50:54 -0400

    cookies prod access

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,23 @@
 import express from 'express';
 import router from './routes/index.mjs';
+import cookieParser from 'cookie-parser';
 
 
 const app = express()
 
-app.use(router);
+
+app.use(cookieParser("helloworld"));
 app.use(express.json())
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (request, response) => {
+    response.cookie('hello', 'world', {maxAge: 30000, signed: true}) 
+    response.status(201).send({msg: "Welcome to the API"})
+});
 
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
-})
+}) 
